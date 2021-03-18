995cc3cc371895b3167708f4e4e9553e3e8ee7a1
SpeedSter <72403623+SpeedSterKawaii@users.noreply.github.com>
2021-03-18 11:05:56 -0400

    Create Whitelist.cpp

---
+++ Whitelist.cpp
@@ -0,0 +1,15 @@
+void CheckWhiteList()
+{
+	DWORD max = 30;
+	DWORD Eeeeee;
+	DWORD DeepPainIsIDANerd;
+	GetVolumeInformation(NULL, NULL, NULL, &Eeeeee, &max, NULL, NULL, NULL);
+	if (Eeeeee == 2389665645) // Remember, this is the Volume ID.
+	{
+		// Your shit thing (means its whitelisted)
+	}
+	else
+	{
+    		// No your not whitelisted nerd.
+	}
+}
